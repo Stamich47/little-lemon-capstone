66cd543802ab8c8003951cd2eb16bb351cc43566
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2024-09-05 12:18:53 -0700

    Create new components for the different sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
 function App() {
-  return <div className="App">Main Page</div>;
+  return (
+    <react>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </react>
+  );
 }
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+export default function Main() {
+  return (
+    <main>
+      <h2>This is the Main Content Area</h2>
+      <p>Some text about some rand stuff and all that jazz.</p>
+    </main>
+  );
+}
